945f1aa3a96d3440ef6c7a59bb9f35225c372da3
Yousuf Tanvir <yousuftanvirk@gmail.com>
2019-09-02 15:54:32 +0500

    fixed

--- test/registration.php
+++ test/registration.php
@@ -7,7 +7,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Unit1_Test',asdasdasdsdaasdasdwwasdsadsaasdsad
-    __DIR__asadsa
-);sadasdasddsasdasasdsa
-asdadsffgfddsfdsfrtrtrtsdssreerddsd
+    'Unit1_Test',
+    __DIR__
+);
